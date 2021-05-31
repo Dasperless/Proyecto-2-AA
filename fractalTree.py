a03593c1fbaa880c9207340f6ed4d0032aef6337
Darío Vargas <dariovm13@gmail.com>
2021-05-31 17:00:53 -0600

    [ADD]: Función para obtener la nota

--- fractalTree.py
+++ fractalTree.py
@@ -1,12 +1,15 @@
-import PIL
-import pygame 
+import pygame
 import math
 import random as r
+import numpy as np
 from PIL import Image
+
 
 class fractalTree:
 	def __init__(self):
 		self.SilhouetteMatrix = []
+		self.FractalMatrix = []
+		self.screen = None
 
 	def windowSettings(self):
 		pygame.init()
@@ -14,31 +17,40 @@ class fractalTree:
 		pygame.display.set_caption("Fractal Tree")
 		self.screen = pygame.display.get_surface()
 
-	def getDataFromSilhouette(self, path):	
+	def getScore(self, silhouetteArr, fractalArr):
+		fractalNpArr = np.array(fractalArr)
+		notEqual = 0
+		for i in range(len(fractalNpArr)):
+			if(silhouetteArr[i] not in fractalNpArr):
+				notEqual += 1
+		return notEqual/len(silhouetteArr)
+
+	def getDataFromSilhouette(self, path):
 		img = Image.open(path)
 		rgbImage = img.convert("RGB")
-		for i in range(1000):
-			x = r.randint(0,599)
-			y = r.randint(0,599)
-			if(rgbImage.getpixel((x,y)) != (255,255,255) and [x,y] not in self.SilhouetteMatrix):
-				self.SilhouetteMatrix.append([x,y])
-		sortedMatrix = sorted(self.SilhouetteMatrix, key=lambda k:[k[1],k[0]])
-		return sortedMatrix
+		im = np.array(rgbImage)
+		black = [0, 0, 0]
+		X, Y = np.where(np.all(im == black, axis=2))
+		self.SilhouetteMatrix = np.column_stack((X, Y))
+		return self.SilhouetteMatrix
 
 	def drawTree(self, x1, y1, angle, forkAng, depth, baseLen, lenDec, baseDiam, diamDec):
+		self.FractalMatrix.append([x1, y1])
 		if depth > 0:
 			x2 = x1 + int(math.cos(math.radians(angle)) * depth * baseLen)
 			y2 = y1 + int(math.sin(math.radians(angle)) * depth * baseLen)
-			pygame.draw.line(self.screen, (255, 255, 255),
-                            (x1, y1), (x2, y2), baseDiam)
+			if(self.screen != None):
+				pygame.draw.line(self.screen, (255, 255, 255),
+								 (x1, y1), (x2, y2), baseDiam)
 			self.drawTree(x2, y2, angle - forkAng, forkAng, depth - 1, baseLen -
-                            lenDec, lenDec, baseDiam-diamDec, diamDec)
+						  lenDec, lenDec, baseDiam-diamDec, diamDec)
 			self.drawTree(x2, y2, angle + forkAng, forkAng, depth - 1, baseLen -
-                            lenDec, lenDec, baseDiam-diamDec, diamDec)
+						  lenDec, lenDec, baseDiam-diamDec, diamDec)
 
 	def showTree(self, x1, y1, angle, forkAngle, depth, baseLen, lenDec, baseDiam, diamDec):
 		self.windowSettings()
-		self.drawTree(x1, y1, angle, forkAngle, depth, baseLen,lenDec, baseDiam, diamDec)
+		self.drawTree(x1, y1, angle, forkAngle, depth,
+					  baseLen, lenDec, baseDiam, diamDec)
 		pygame.display.flip()
 		while True:
 			self.input(pygame.event.wait())
@@ -46,7 +58,9 @@ class fractalTree:
 	def input(self, event):
 		if event.type == pygame.QUIT:
 			quit()
+
+
 a = fractalTree()
 m = a.getDataFromSilhouette("silueta.gif")
-#tener cuidado con la profundidad con rangos mayores a 25 se traba
-# a.showTree(300, 600, -90, 20,20, 7, 1, 2, 0)
+a.drawTree(300, 599, -90, 10, 9, 10, 1, 2, 0)
+print(a.getScore(m, a.FractalMatrix))
